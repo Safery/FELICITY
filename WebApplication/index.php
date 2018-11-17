082917f483ee5dacf5287ce5612bc22e48715e68
Safery Hossain <janingpower@yahoo.com>
2018-11-17 11:33:54 -0500

    Added stuff

--- WebApplication/index.php
+++ WebApplication/index.php
@@ -1,3 +1,7 @@
+<?php
+  require_once "php/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -460,8 +464,15 @@
                                                 <i class="zmdi zmdi-account-o"></i>
                                             </div>
                                             <div class="text">
-                                                <h2>10368</h2>
-                                                <span>members online</span>
+                                              <?php
+                                                $sql = "SELECT COUNT(*) AS `total` FROM `patient`";
+                                                $result = $conn->query($sql);
+                                                // output data of each row
+                                                while($row = $result->fetch_assoc()) {
+                                                    echo "<h2>". $row["total"] . "<h2/>";
+                                                }
+                                              ?>
+                                                <span>Patients</span>
                                             </div>
                                         </div>
                                         <div class="overview-chart">
@@ -478,8 +489,15 @@
                                                 <i class="zmdi zmdi-shopping-cart"></i>
                                             </div>
                                             <div class="text">
-                                                <h2>388,688</h2>
-                                                <span>items solid</span>
+                                              <?php
+                                                $sql = "SELECT COUNT(*) AS `total` FROM `medication`";
+                                                $result = $conn->query($sql);
+                                                // output data of each row
+                                                while($row = $result->fetch_assoc()) {
+                                                    echo "<h2>". $row["total"] . "<h2/>";
+                                                }
+                                              ?>
+                                              <span>Medicine</span>
                                             </div>
                                         </div>
                                         <div class="overview-chart">
@@ -496,8 +514,15 @@
                                                 <i class="zmdi zmdi-calendar-note"></i>
                                             </div>
                                             <div class="text">
-                                                <h2>1,086</h2>
-                                                <span>this week</span>
+                                              <?php
+                                                $sql = "SELECT COUNT(*) AS `total` FROM medication WHERE medication_intake='ed'";
+                                                $result = $conn->query($sql);
+                                                // output data of each row
+                                                while($row = $result->fetch_assoc()) {
+                                                    echo "<h2>". $row["total"] . "<h2/>";
+                                                }
+                                              ?>
+                                              <span>Require Attention</span>
                                             </div>
                                         </div>
                                         <div class="overview-chart">
@@ -506,100 +531,18 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-6 col-lg-3">
-                                <div class="overview-item overview-item--c4">
-                                    <div class="overview__inner">
-                                        <div class="overview-box clearfix">
-                                            <div class="icon">
-                                                <i class="zmdi zmdi-money"></i>
-                                            </div>
-                                            <div class="text">
-                                                <h2>$1,060,386</h2>
-                                                <span>total earnings</span>
-                                            </div>
-                                        </div>
-                                        <div class="overview-chart">
-                                            <canvas id="widgetChart4"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="au-card recent-report">
-                                    <div class="au-card-inner">
-                                        <h3 class="title-2">recent reports</h3>
-                                        <div class="chart-info">
-                                            <div class="chart-info__left">
-                                                <div class="chart-note">
-                                                    <span class="dot dot--blue"></span>
-                                                    <span>products</span>
-                                                </div>
-                                                <div class="chart-note mr-0">
-                                                    <span class="dot dot--green"></span>
-                                                    <span>services</span>
-                                                </div>
-                                            </div>
-                                            <div class="chart-info__right">
-                                                <div class="chart-statis">
-                                                    <span class="index incre">
-                                                        <i class="zmdi zmdi-long-arrow-up"></i>25%</span>
-                                                    <span class="label">products</span>
-                                                </div>
-                                                <div class="chart-statis mr-0">
-                                                    <span class="index decre">
-                                                        <i class="zmdi zmdi-long-arrow-down"></i>10%</span>
-                                                    <span class="label">services</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="recent-report__chart">
-                                            <canvas id="recent-rep-chart"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="au-card chart-percent-card">
-                                    <div class="au-card-inner">
-                                        <h3 class="title-2 tm-b-5">char by %</h3>
-                                        <div class="row no-gutters">
-                                            <div class="col-xl-6">
-                                                <div class="chart-note-wrap">
-                                                    <div class="chart-note mr-0 d-block">
-                                                        <span class="dot dot--blue"></span>
-                                                        <span>products</span>
-                                                    </div>
-                                                    <div class="chart-note mr-0 d-block">
-                                                        <span class="dot dot--red"></span>
-                                                        <span>services</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-6">
-                                                <div class="percent-chart">
-                                                    <canvas id="percent-chart"></canvas>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                         <div class="row">
                             <div class="col-lg-9">
-                                <h2 class="title-1 m-b-25">Earnings By Items</h2>
+                                <h2 class="title-1 m-b-25">Today Medicine</h2>
                                 <div class="table-responsive table--no-card m-b-40">
                                     <table class="table table-borderless table-striped table-earning">
                                         <thead>
                                             <tr>
-                                                <th>date</th>
-                                                <th>order ID</th>
-                                                <th>name</th>
-                                                <th class="text-right">price</th>
-                                                <th class="text-right">quantity</th>
-                                                <th class="text-right">total</th>
+                                                <th>Name</th>
+                                                <th>Time</th>
+                                                <th>Medicine Name</th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -607,113 +550,44 @@
                                                 <td>2018-09-29 05:57</td>
                                                 <td>100398</td>
                                                 <td>iPhone X 64Gb Grey</td>
-                                                <td class="text-right">$999.00</td>
-                                                <td class="text-right">1</td>
-                                                <td class="text-right">$999.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-28 01:22</td>
                                                 <td>100397</td>
                                                 <td>Samsung S8 Black</td>
-                                                <td class="text-right">$756.00</td>
-                                                <td class="text-right">1</td>
-                                                <td class="text-right">$756.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-27 02:12</td>
                                                 <td>100396</td>
                                                 <td>Game Console Controller</td>
-                                                <td class="text-right">$22.00</td>
-                                                <td class="text-right">2</td>
-                                                <td class="text-right">$44.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-26 23:06</td>
                                                 <td>100395</td>
                                                 <td>iPhone X 256Gb Black</td>
-                                                <td class="text-right">$1199.00</td>
-                                                <td class="text-right">1</td>
-                                                <td class="text-right">$1199.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-25 19:03</td>
                                                 <td>100393</td>
                                                 <td>USB 3.0 Cable</td>
-                                                <td class="text-right">$10.00</td>
-                                                <td class="text-right">3</td>
-                                                <td class="text-right">$30.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-29 05:57</td>
                                                 <td>100392</td>
                                                 <td>Smartwatch 4.0 LTE Wifi</td>
-                                                <td class="text-right">$199.00</td>
-                                                <td class="text-right">6</td>
-                                                <td class="text-right">$1494.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-24 19:10</td>
                                                 <td>100391</td>
                                                 <td>Camera C430W 4k</td>
-                                                <td class="text-right">$699.00</td>
-                                                <td class="text-right">1</td>
-                                                <td class="text-right">$699.00</td>
                                             </tr>
                                             <tr>
                                                 <td>2018-09-22 00:43</td>
                                                 <td>100393</td>
                                                 <td>USB 3.0 Cable</td>
-                                                <td class="text-right">$10.00</td>
-                                                <td class="text-right">3</td>
-                                                <td class="text-right">$30.00</td>
                                             </tr>
                                         </tbody>
                                     </table>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
-                                <h2 class="title-1 m-b-25">Top countries</h2>
-                                <div class="au-card au-card--bg-blue au-card-top-countries m-b-40">
-                                    <div class="au-card-inner">
-                                        <div class="table-responsive">
-                                            <table class="table table-top-countries">
-                                                <tbody>
-                                                    <tr>
-                                                        <td>United States</td>
-                                                        <td class="text-right">$119,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Australia</td>
-                                                        <td class="text-right">$70,261.65</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>United Kingdom</td>
-                                                        <td class="text-right">$46,399.22</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Turkey</td>
-                                                        <td class="text-right">$35,364.90</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Germany</td>
-                                                        <td class="text-right">$20,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>France</td>
-                                                        <td class="text-right">$10,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Australia</td>
-                                                        <td class="text-right">$5,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Italy</td>
-                                                        <td class="text-right">$1639.32</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
--- WebApplication/php/config.php
+++ WebApplication/php/config.php
@@ -6,5 +6,11 @@ define('DB_USERNAME', 'janingpo_lina');
 define('DB_PASSWORD', 'safery123');
 define('DB_NAME', 'janingpo_lisakun');
 
+$servername = "jannatulferdous.ca";
+$username = "janingpo_lina";
+$password = "safery123";
+$dbname = "janingpo_lisakun";
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = new mysqli($servername, $username, $password, $dbname);
