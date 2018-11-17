29b0049659eeb4ebbd0efc0f096a05ac4532c8b2
Safery Hossain <janingpower@yahoo.com>
2018-11-17 01:00:28 -0500

    las

--- WebApplication/php/config.php
+++ WebApplication/php/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'felicity');
+define('DB_SERVER', 'jannatulferdous.ca');
+define('DB_USERNAME', 'janingpo_lina');
+define('DB_PASSWORD', 'safery123');
+define('DB_NAME', 'janingpo_lisakun');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
