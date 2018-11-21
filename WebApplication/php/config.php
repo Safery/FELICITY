0f8f3df1059505d707cc08e85a1baebe4d405a16
Safery Hossain <janingpower@yahoo.com>
2018-11-21 13:28:04 -0500

    Update config.php

--- WebApplication/php/config.php
+++ WebApplication/php/config.php
@@ -1,15 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'jannatulferdous.ca');
-define('DB_USERNAME', 'janingpo_lina');
-define('DB_PASSWORD', 'safery123');
-define('DB_NAME', 'janingpo_lisakun');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
-$servername = "jannatulferdous.ca";
-$username = "janingpo_lina";
-$password = "safery123";
-$dbname = "janingpo_lisakun";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
